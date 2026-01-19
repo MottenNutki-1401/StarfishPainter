05b9793b3f4295c4462908c33f19da5c052599fe
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-01-20 00:56:21 +0800

    Fix custom navbar, need to replace the layoutfor precise designing goal

--- home.java
+++ home.java
@@ -17,7 +17,6 @@ public class home {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //to center the window (frame) we can pass it to null so swing will center it instead of putting it next to a component (c)
         frame.setLocationRelativeTo(null);
-        frame.setVisible(true); // this is best to be in last part of the frame so we can make sure other methods will work
         
 
         //create a JPanel for our customwinow
@@ -25,8 +24,11 @@ public class home {
 
         //inside this mainPanel (the container we created) we will put the design of the window such as the tittle bar
         JPanel TittleBar = new JPanel();
-        TittleBar.setBackground(new Color(30,144,255)); // set background color of tittle bar)
-        TittleBar.setPreferredSize(new Dimension (800,400));
+
+        //we can use decode method to set custom colors using hex color codes
+        TittleBar.setBackground(Color.decode("#FF8989")); // set background color of tittle bar
+        TittleBar.setPreferredSize(new Dimension (800,40));
+        TittleBar.setLayout(new BorderLayout()); // set layout of tittle bar to BorderLayout
 
         //Then we can label the "TittleBar" we created using a JLabel
         JLabel appName = new JLabel ("Starfish Painter");
@@ -35,10 +37,23 @@ public class home {
         //Then we can add buttons to our window 
         JButton closeButton = new JButton ("X");
         closeButton.setForeground(Color.BLACK);
+        //design for the close button
+        closeButton.setBackground(Color.white);
+        closeButton.setForeground(Color.BLACK);
         //logic for our close button
         closeButton.addActionListener(e -> frame.dispose() ); //(frame) here is the main JFrame we created 
+
+        //In this section we will place the components we just created using a BorderLayout
+        //So far we craeted two components which is a tiitle bar and a close button
+        TittleBar.add(appName,BorderLayout.WEST);
         
 
+        //visibility logic
+         mainPanel.add(TittleBar, BorderLayout.NORTH); // add the tittle bar to the main panel at the top (NORTH)
+         TittleBar.add(closeButton, BorderLayout.EAST);
+         TittleBar.add(appName, BorderLayout.WEST);
+         frame.setContentPane(mainPanel); //this line is very important so we can see the main panel
+         frame.setVisible(true); // this is best to be in last part of the frame so we can make sure other methods will work
 
     }
     
